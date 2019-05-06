bd10b695521df68b44f45d3bbf744d34c8437ade
Shiqan <ferron.saan@live.nl>
2019-05-06 18:58:16 +0200

    Add HasLevelStreamingFixes() to header

--- src/FortniteReplayReader.Core/Models/Header.cs
+++ src/FortniteReplayReader.Core/Models/Header.cs
@@ -11,14 +11,23 @@ namespace FortniteReplayReader.Core.Models
         public uint NetworkChecksum { get; set; }
         public EngineNetworkVersionHistory EngineNetworkVersionHistory { get; set; }
         public uint GameNetworkProtocolVersion { get; set; }
-        public string Guid { get; set; } = "";
-        public uint Major { get; set; }
-        public uint Minor { get; set; }
-        public uint Patch { get; set; }
+        public string Guid { get; set; }
+        public ushort Major { get; set; }
+        public ushort Minor { get; set; }
+        public ushort Patch { get; set; }
         public uint Changelist { get; set; }
-        public string Branch { get; set; } = "";
+        public string Branch { get; set; }
         public (string, uint)[] LevelNamesAndTimes { get; set; }
         public ReplayHeaderFlags Flags { get; set; }
         public string[] GameSpecificData { get; set; }
+
+        /// <summary>
+        /// Returns whether or not this replay was recorded / is playing with Level Streaming fixes.
+        /// see https://github.com/EpicGames/UnrealEngine/blob/811c1ce579564fa92ecc22d9b70cbe9c8a8e4b9a/Engine/Source/Runtime/Engine/Classes/Engine/DemoNetDriver.h#L693
+        /// </summary>
+        public bool HasLevelStreamingFixes()
+        {
+            return Flags >= ReplayHeaderFlags.HasStreamingFixes;
+        }
     }
 }
